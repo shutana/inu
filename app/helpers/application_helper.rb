f5aa0f494c946ffad5c3011b4e4d47b768ca09b6
Shusei Tanaka <>
2021-01-16 11:21:08 +0900

    メイン機能実装（複数あり）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   #def simple_time(time)
     #time.strftime("%Y-%m-%d<br />%H:%M:%S")
   #end
+
+  def anniversary_date(dating_start_date)
+    today = Date.current
+    dead_line = Date.new(dating_start_date.strftime("%Y").to_i, dating_start_date.strftime("%m").to_i ,dating_start_date.strftime("%d").to_i)
+    remain = (today - dead_line).to_i
+  end
 end
